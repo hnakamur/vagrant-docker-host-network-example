1a2900da4abb3802263539809d9e3df603862726
Hiroaki Nakamura <hnakamur@gmail.com>
2014-11-11 14:26:50 +0900

    Make MySQL data persistent and make pid and socket files not persistent

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -i --net=host -p 3306:3006 -v /var/host_lib/mysql:/var/lib/mysql -t hnakamur/mysql /bin/bash
+docker run -d --net=host -p 3306:3006 -v /var/host_lib/mysql:/var/lib/mysql -t hnakamur/mysql
