8b1ee9f4cd8362957e2ab82f0fe7ee49896527ef
Hiroaki Nakamura <hnakamur@gmail.com>
2014-11-11 17:48:14 +0900

    Need to restart docker.service after restarting firewalld.service

--- setup_docker_host.sh
+++ setup_docker_host.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 yum install -y bind-utils
+
+# configure firewall
 if ! (iptables-save | grep '^-A IN_public_allow -p tcp -m tcp --dport 3000 -m conntrack --ctstate NEW -j ACCEPT$'); then
   firewall-cmd --permanent --add-port=3000/tcp
   systemctl restart firewalld.service
+
+  # NOTE: Without restarting docker service, I could not resolve hostnames on the Internet
+  # within a container.
+  systemctl restart docker.service
 fi
